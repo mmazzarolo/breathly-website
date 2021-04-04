65ca28f9017e6b79384af9b1a3a60df43555e21f
mmazzarolo <me@mazzarolomatteo.com>
2021-04-04 11:58:00 +0200

    Improved accessibility & SEO

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,37 @@ const postcssCustomProperties = require("postcss-custom-properties");
 const cssnano = require("cssnano");
 const imagemin = require("gulp-imagemin");
 
-gulp.task("handleCssFile", function() {
+gulp.task("handleCssFile", gulp.series(function() {
   return gulp
     .src("src/index.css")
     .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer(), postcssCustomProperties(), cssnano()]))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("./build"));
-});
+}));
 
-gulp.task("handleHtmlFile", function() {
+gulp.task("handleHtmlFile", gulp.series(function() {
   return gulp.src("src/index.html").pipe(gulp.dest("build/"));
-});
+}));
 
-gulp.task("handleImages", function() {
+gulp.task("handleImages", gulp.series(function() {
   return gulp
     .src("src/images/*")
     .pipe(imagemin())
     .pipe(gulp.dest("./build/images"));
-});
+}));
 
-gulp.task("copyRemainingFiles", function() {
+gulp.task("copyRemainingFiles", gulp.series(function() {
   return gulp
     .src(["src/favicons/**/*", "src/manifest.json", "src/privacy"], {
       base: "src"
     })
     .pipe(gulp.dest("./build"));
-});
+}));
 
-gulp.task("default", [
+gulp.task("default", gulp.parallel([
   "handleCssFile",
   "handleHtmlFile",
   "handleImages",
   "copyRemainingFiles"
-]);
+]));
